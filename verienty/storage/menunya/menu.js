75e49d947ac936197b03819cf035a5bba1dc6d71
Aztecs444 <Aztecs395@gmail.com>
2022-05-08 16:55:05 +0700

    KONTOL

---
+++ verienty/storage/menunya/menu.js
@@ -0,0 +1,616 @@
+const menunya = (prefix) => {
+return`╔─❒「 𝑂𝑤𝑛𝑒𝑟 」❒
+┃冬 ${prefix}leave
+║冬 ${prefix}autorespon
+┃冬 ${prefix}bc [ teks ]
+║冬 ${prefix}tobc [ reply sticker/audio ]
+┃冬 ${prefix}creategrup [ nama|@tag ]
+║冬 ${prefix}culik [ url ]
+┃冬 ${prefix}jadibot
+║冬 ${prefix}exif [ nama|author ]
+┃冬 ${prefix}self
+║冬 ${prefix}demoteall
+┃冬 ${prefix}listrespon
+║冬 ${prefix}mute
+┃冬 ${prefix}nsfw
+║冬 ${prefix}offline [ alasan ]
+┃冬 ${prefix}online
+║冬 ${prefix}public
+┃冬 ${prefix}restart
+║冬 ${prefix}readall
+┃冬 ${prefix}unreadall
+║冬 ${prefix}archive
+┃冬 ${prefix}unarchive
+║冬 ${prefix}pin
+┃冬 ${prefix}unpin
+║冬 ${prefix}setbio [ teks ]
+┃冬 ${prefix}setname [ teks ]
+║冬 ${prefix}promoteall
+┃冬 ${prefix}setprofile [ reply image ]
+║冬 ${prefix}bukatime [ detik/menit/jam ]
+┃冬 ${prefix}tutuptime  [ detik/menit/jam ]
+║冬 ${prefix}leavetime [ detik/menit/jam ]
+┃冬 ${prefix}stopjadibot
+║冬 ${prefix}autoread
+┃冬 ${prefix}spamsw [ teks|jumlah ]
+║冬 ${prefix}upswteks [ teks ]
+┃冬 ${prefix}upswlokasi [ teks ]
+║冬 ${prefix}upswsticker [ reply sticker ]
+┃冬 ${prefix}upswaudio [ reply audio ]
+║冬 ${prefix}upswvoice [ reply audio ]
+┃冬 ${prefix}upswvideo [ with caption ]
+║冬 ${prefix}upswgif [ with caption ]
+┃冬 ${prefix}upswimage [ with caption ]
+╚❏
+
+╔─❒「 𝐺𝑟𝑜𝑢𝑝 」❒
+┃冬 ${prefix}ad [ reply pesan ]
+║冬 ${prefix}add 
+┃冬 ${prefix}promote [ @tag ]
+║冬 ${prefix}demote [ @tag ]
+┃冬 ${prefix}welcome
+║冬 ${prefix}antibug
+┃冬 ${prefix}antidelete
+║冬 ${prefix}antilink
+┃冬 ${prefix}caripesan [ pesan|pesan ]
+║冬 ${prefix}fitnah [ @tag|pesan|balasan ]
+┃冬 ${prefix}getdeskgc
+║冬 ${prefix}grup
+┃冬 ${prefix}getpp [ @tag ]
+║冬 ${prefix}hacked
+┃冬 ${prefix}hidetag [ teks ]
+║冬 ${prefix}infouser [ reply pesan ]
+┃冬 ${prefix}kontak [ @tag|nama ]
+║冬 ${prefix}kick [ @tag ]
+┃冬 ${prefix}kik [ reply pesan ]
+║冬 ${prefix}linkgrup
+┃冬 ${prefix}listadmin
+║冬 ${prefix}listonline
+┃冬 ${prefix}revoke
+║冬 ${prefix}setdeskgc
+┃冬 ${prefix}setnamegc
+║冬 ${prefix}sider [ reply pesan ]
+┃冬 ${prefix}sticktag [ reply sticker ]
+║冬 ${prefix}spam [ teks|jumlah ]
+┃冬 ${prefix}tag [ @tag ]
+║冬 ${prefix}tagall [ teks ]
+┃冬 ${prefix}tagme
+║冬 ${prefix}tospam
+┃冬 ${prefix}totag
+║冬 ${prefix}setppgrup [ reply image ] 
+╚❏
+
+╔─❒「 𝑀𝑎𝑘𝑒𝑟 」❒
+┃冬 ${prefix}nulis [ teks ]
+║冬 ${prefix}neon [ teks ]
+┃冬 ${prefix}bengekmaker [ teks ]
+║冬 ${prefix}terbaikmaker [ teks ]
+┃冬 ${prefix}memekertas [ teks ]
+║冬 ${prefix}greenneon [ teks ]
+┃冬 ${prefix}futureneon [ teks ]
+║冬 ${prefix}sandwriting [ teks ]
+┃冬 ${prefix}sandsummer [ teks ]
+║冬 ${prefix}sandengraved [ teks ]
+┃冬 ${prefix}metaldark [ teks ]
+║冬 ${prefix}neonlight [ teks ]
+┃冬 ${prefix}holographic [ teks ]
+║冬 ${prefix}text1917 [ teks ]
+┃冬 ${prefix}minion [ teks ]
+║冬 ${prefix}deluxesilver [ teks ]
+┃冬 ${prefix}newyearcard [ teks ]
+║冬 ${prefix}bloodfrosted [ teks ]
+┃冬 ${prefix}halloween [ teks ]
+║冬 ${prefix}jokerlogo [ teks ]
+┃冬 ${prefix}fireworksparkle [ teks ]
+║冬 ${prefix}natureleaves [ teks ]
+┃冬 ${prefix}bokeh [ teks ]
+║冬 ${prefix}toxic [ teks ]
+┃冬 ${prefix}strawberry [ teks ]
+║冬 ${prefix}box3d [ teks ]
+┃冬 ${prefix}roadwarning [ teks ]
+║冬 ${prefix}breakwall [ teks ]
+┃冬 ${prefix}icecold [ teks ]
+║冬 ${prefix}luxury [ teks ]
+┃冬 ${prefix}cloud [ teks ]
+║冬 ${prefix}summersand [ teks ]
+┃冬 ${prefix}horrorblood [ teks ]
+║冬 ${prefix}thunder [ teks ]
+┃冬 ${prefix}pornhub [ teks ]
+║冬 ${prefix}glitch [ teks ]
+┃冬 ${prefix}avenger [ teks ]
+║冬 ${prefix}space [ teks ]
+┃冬 ${prefix}ninjalogo [ teks ]
+║冬 ${prefix}marvelstudio [ teks ]
+┃冬 ${prefix}lionlogo [ teks ]
+║冬 ${prefix}wolflogo [ teks ]
+┃冬 ${prefix}steel3d [ teks ]
+║冬 ${prefix}wallgravity [ teks ]
+┃冬 ${prefix}shadow [ teks ]
+║冬 ${prefix}cup [ teks ]
+┃冬 ${prefix}cup1 [ teks ]
+║冬 ${prefix}romance [ teks ]
+┃冬 ${prefix}smoke [ teks ]
+║冬 ${prefix}burnpaper [ teks ]
+┃冬 ${prefix}lovemessage [ teks ]
+║冬 ${prefix}undergrass [ teks ]
+┃冬 ${prefix}love [ teks ]
+║冬 ${prefix}coffe [ teks ]
+┃冬 ${prefix}woodheart [ teks ]
+║冬 ${prefix}woodenboard [ teks ]
+┃冬 ${prefix}summer3d [ teks ]
+║冬 ${prefix}wolfmetal [ teks ]
+┃冬 ${prefix}nature3d [ teks ]
+║冬 ${prefix}underwater [ teks ]
+┃冬 ${prefix}golderrose [ teks ]
+║冬 ${prefix}summernature [ teks ]
+┃冬 ${prefix}fallleaves [ teks ]
+║冬 ${prefix}flamming [ teks ]
+┃冬 ${prefix}harrypotter [ teks ]
+║冬 ${prefix}carvedwood [ teks ]
+┃冬 ${prefix}arcade8bit [ teks ]
+║冬 ${prefix}battlefield4 [ teks ]
+┃冬 ${prefix}pubg [ teks ]
+║冬 ${prefix}wetglass [ teks ]
+┃冬 ${prefix}multicolor3d [ teks ]
+║冬 ${prefix}watercolor [ teks ]
+┃冬 ${prefix}luxurygold [ teks ]
+║冬 ${prefix}galaxywallpaper [ teks ]
+┃冬 ${prefix}lighttext [ teks ]
+║冬 ${prefix}beautifulflower [ teks ]
+┃冬 ${prefix}puppycute [ teks ]
+║冬 ${prefix}royaltext [ teks ]
+┃冬 ${prefix}birthdaycake [ teks ]
+║冬 ${prefix}galaxystyle [ teks ]
+┃冬 ${prefix}hologram3d [ teks ]
+║冬 ${prefix}greenneon [ teks ]
+┃冬 ${prefix}glossychrome [ teks ]
+║冬 ${prefix}greenbush [ teks ]
+┃冬 ${prefix}metallogo [ teks ]
+║冬 ${prefix}noeltext [ teks ]
+┃冬 ${prefix}glittergold [ teks ]
+║冬 ${prefix}textcake [ teks ]
+┃冬 ${prefix}starsnight [ teks ]
+║冬 ${prefix}wooden3d [ teks ]
+┃冬 ${prefix}textbyname [ teks ]
+║冬 ${prefix}writegalacy [ teks ]
+┃冬 ${prefix}galaxybat [ teks ]
+║冬 ${prefix}snow3d [ teks ]
+┃冬 ${prefix}birthdayday [ teks ]
+║冬 ${prefix}goldplaybutton [ teks ]
+┃冬 ${prefix}silverplaybutton [ teks ]
+║冬 ${prefix}freefire [ teks ]
+┃冬 ${prefix}shadow [ teks ]
+╚❏
+
+╔─❒「 𝑆𝑡𝑖𝑐𝑘𝑒𝑟 」❒
+┃冬 ${prefix}attp
+║冬 ${prefix}ttp
+┃冬 ${prefix}ttp2
+║冬 ${prefix}ttp3
+┃冬 ${prefix}ttp4
+║冬 ${prefix}sticker
+┃冬 ${prefix}swm
+║冬 ${prefix}take
+┃冬 ${prefix}dadu
+║冬 ${prefix}colong
+┃冬 ${prefix}toimg [ reply sticker ]
+║冬 ${prefix}doge
+┃冬 ${prefix}gura
+║冬 ${prefix}semoji
+┃冬 ${prefix}patrick
+║冬 ${prefix}animestick
+┃冬 ${prefix}stickertele
+║冬 ${prefix}nobg
+╚❏
+
+╔─❒「 𝐶𝑜𝑛𝑣𝑒𝑟𝑡 」❒
+┃冬 ${prefix}balik [ reply audio ]
+║冬 ${prefix}bass [ reply audio ]
+┃冬 ${prefix}detikvn [ reply audio ]
+║冬 ${prefix}detikvideo [ reply audio ]
+┃冬 ${prefix}gemuk [ reply audio ]
+║冬 ${prefix}robot [ reply audio ]
+┃冬 ${prefix}tupai [ reply audio ]
+╚❏
+
+╔─❒「 𝐼𝑠𝑙𝑎𝑚𝑖 」❒
+┃冬 ${prefix}kisahnabi [ nama nabi ]
+║冬 ${prefix}jadwalsholat [ nama kota ]
+┃冬 ${prefix}listsurah
+║冬 ${prefix}alquran [ 1/10 ]
+┃冬 ${prefix}asmaulhusna
+║冬 ${prefix}alquranaudio [ 1/10 ]
+╚❏
+
+╔─❒「 𝐹𝑢𝑛 」❒
+┃冬 ${prefix}bisakah
+║冬 ${prefix}cekpintar
+┃冬 ${prefix}cekganteng
+║冬 ${prefix}cekcantik
+┃冬 ${prefix}cekmati
+║冬 ${prefix}dare
+┃冬 ${prefix}delttc
+║冬 ${prefix}fdeface 
+┃冬 ${prefix}fitnahpc
+║冬 ${prefix}hbd
+┃冬 ${prefix}herolist
+║冬 ${prefix}herodetail
+┃冬 ${prefix}jadian
+║冬 ${prefix}kapankah
+┃冬 ${prefix}slot
+║冬 ${prefix}suit
+┃冬 ${prefix}tictactoe
+║冬 ${prefix}truth
+┃冬 ${prefix}ngewe
+║冬 ${prefix}bego
+┃冬 ${prefix}babi
+║冬 ${prefix}tolol
+┃冬 ${prefix}pinter
+║冬 ${prefix}pintar
+┃冬 ${prefix}asu
+║冬 ${prefix}bodoh
+┃冬 ${prefix}gay
+║冬 ${prefix}lesby
+┃冬 ${prefix}bajingan
+║冬 ${prefix}jancok
+┃冬 ${prefix}anjing
+║冬 ${prefix}ngentod
+┃冬 ${prefix}ngentot
+║冬 ${prefix}monyet
+┃冬 ${prefix}mastah
+║冬 ${prefix}newbie
+┃冬 ${prefix}bangsat
+║冬 ${prefix}bangke
+┃冬 ${prefix}sange
+║冬 ${prefix}sangean
+┃冬 ${prefix}dakjal
+║冬 ${prefix}horny
+┃冬 ${prefix}wibu
+║冬 ${prefix}puki
+┃冬 ${prefix}pantex
+║冬 ${prefix}pantek
+┃冬 ${prefix}exo
+║冬 ${prefix}estetic
+┃冬 ${prefix}meme
+║冬 ${prefix}tebakgambar
+┃冬 ${prefix}pantun
+╚❏
+
+╔─❒「 𝑆𝑜𝑢𝑛𝑑 」❒
+┃冬 ${prefix}sound1
+║冬 ${prefix}sound2
+┃冬 ${prefix}sound3
+║冬 ${prefix}sound4
+┃冬 ${prefix}sound5
+║冬 ${prefix}sound6
+┃冬 ${prefix}sound7
+║冬 ${prefix}sound8
+┃冬 ${prefix}sound9
+║冬 ${prefix}sound10
+┃冬 ${prefix}sound11
+║冬 ${prefix}sound12
+┃冬 ${prefix}sound13
+║冬 ${prefix}sound14
+┃冬 ${prefix}sound15
+║冬 ${prefix}sound16
+┃冬 ${prefix}sound17
+║冬 ${prefix}sound18
+┃冬 ${prefix}sound19
+║冬 ${prefix}sound20
+┃冬 ${prefix}sound21
+║冬 ${prefix}sound22
+┃冬 ${prefix}sound23
+║冬 ${prefix}sound24
+┃冬 ${prefix}sound25
+║冬 ${prefix}sound26
+┃冬 ${prefix}sound27
+║冬 ${prefix}sound28
+┃冬 ${prefix}sound29
+║冬 ${prefix}sound30
+┃冬 ${prefix}sound31
+║冬 ${prefix}sound32
+┃冬 ${prefix}sound33
+║冬 ${prefix}sound34
+┃冬 ${prefix}sound35
+║冬 ${prefix}sound36
+┃冬 ${prefix}sound37
+║冬 ${prefix}sound38
+┃冬 ${prefix}sound39
+║冬 ${prefix}sound40
+┃冬 ${prefix}sound41
+║冬 ${prefix}sound42
+┃冬 ${prefix}sound43
+║冬 ${prefix}sound44
+┃冬 ${prefix}sound45
+║冬 ${prefix}sound46
+┃冬 ${prefix}sound47
+║冬 ${prefix}sound48
+┃冬 ${prefix}sound49
+║冬 ${prefix}sound50
+┃冬 ${prefix}sound51
+║冬 ${prefix}sound52
+┃冬 ${prefix}sound53
+║冬 ${prefix}sound54
+┃冬 ${prefix}sound55
+║冬 ${prefix}sound56
+┃冬 ${prefix}sound57
+║冬 ${prefix}sound58
+┃冬 ${prefix}sound59
+║冬 ${prefix}sound60
+┃冬 ${prefix}sound61
+║冬 ${prefix}sound62
+┃冬 ${prefix}sound63
+║冬 ${prefix}sound64
+┃冬 ${prefix}sound65
+║冬 ${prefix}sound66
+┃冬 ${prefix}sound67
+║冬 ${prefix}sound68
+┃冬 ${prefix}sound69
+║冬 ${prefix}sound70
+┃冬 ${prefix}sound71
+║冬 ${prefix}sound72
+┃冬 ${prefix}sound73
+║冬 ${prefix}sound74
+┃冬 ${prefix}sound75
+║冬 ${prefix}sound76
+┃冬 ${prefix}sound77
+║冬 ${prefix}sound78
+┃冬 ${prefix}sound79
+║冬 ${prefix}sound80
+┃冬 ${prefix}sound81
+║冬 ${prefix}sound82
+┃冬 ${prefix}sound83
+║冬 ${prefix}sound84
+┃冬 ${prefix}sound85
+║冬 ${prefix}sound86
+┃冬 ${prefix}sound87
+║冬 ${prefix}sound88
+┃冬 ${prefix}sound89
+║冬 ${prefix}sound90
+┃冬 ${prefix}sound91
+║冬 ${prefix}sound92
+┃冬 ${prefix}sound93
+║冬 ${prefix}sound94
+┃冬 ${prefix}sound95
+║冬 ${prefix}sound96
+┃冬 ${prefix}sound97
+║冬 ${prefix}sound98
+┃冬 ${prefix}sound99
+║冬 ${prefix}sound100
+╚❏
+
+╔─❒「 𝐴𝑠𝑢𝑝𝑎𝑛 」❒
+┃冬 ${prefix}cogan
+║冬 ${prefix}cecan
+┃冬 ${prefix}random
+║冬 ${prefix}cecanrandom
+┃冬 ${prefix}cecanvietnam
+║冬 ${prefix}cecanmalaysia
+┃冬 ${prefix}cecankorea
+║冬 ${prefix}cecanindonesia
+┃冬 ${prefix}cecanjepang
+║冬 ${prefix}cecanthailand
+┃冬 ${prefix}hijaber
+║冬 ${prefix}santuy
+┃冬 ${prefix}ukhti
+║冬 ${prefix}bocil
+┃冬 ${prefix}ghea
+║冬 ${prefix}rika
+┃冬 ${prefix}blackpink
+║冬 ${prefix}bts
+┃冬 ${prefix}bokep
+║冬 ${prefix}cosplay
+╚❏
+
+╔─❒「 𝑆𝑡𝑜𝑟𝑎𝑔𝑒 」❒
+┃冬 ${prefix}addrespon [ tanya|jawab ]
+║冬 ${prefix}delrespon [ teks ]
+┃冬 ${prefix}addcmd [ teks reply sticker ]
+║冬 ${prefix}getscmd
+┃冬 ${prefix}delcmd [ reply sticker ]
+║冬 ${prefix}addimage [ reply image ]
+┃冬 ${prefix}addsticker [ reply sticker ]
+║冬 ${prefix}addvn [ reply audio ]
+┃冬 ${prefix}delimage [ teks ]
+║冬 ${prefix}delsticker [ tek ]
+┃冬 ${prefix}delvn [ teks]
+║冬 ${prefix}liststicker
+┃冬 ${prefix}listvn
+║冬 ${prefix}listcmd
+┃冬 ${prefix}listimage
+╚❏
+
+╔─❒「 𝐴𝑛𝑖𝑚𝑒 」❒
+┃冬 ${prefix}loli
+║冬 ${prefix}husbu                     
+┃冬 ${prefix}waifu
+║冬 ${prefix}wallnime
+┃冬 ${prefix}kanna
+║冬 ${prefix}neko       
+┃冬 ${prefix}sagiri
+║冬 ${prefix}megumin
+┃冬 ${prefix}elf
+║冬 ${prefix}shota
+┃冬 ${prefix}elaina
+║冬 ${prefix}shinobu
+┃冬 ${prefix}art       
+║冬 ${prefix}bully
+┃冬 ${prefix}cuddle
+║冬 ${prefix}cry
+┃冬 ${prefix}hug
+║冬 ${prefix}awoo
+┃冬 ${prefix}kiss
+║冬 ${prefix}lick
+┃冬 ${prefix}pat
+║冬 ${prefix}smug
+┃冬 ${prefix}bonk
+║冬 ${prefix}yeet
+┃冬 ${prefix}blush
+║冬 ${prefix}smile
+┃冬 ${prefix}wave
+║冬 ${prefix}highfive
+┃冬 ${prefix}handhold
+║冬 ${prefix}nom
+┃冬 ${prefix}bite
+║冬 ${prefix}glomp
+┃冬 ${prefix}kill
+║冬 ${prefix}slap
+┃冬 ${prefix}happy
+║冬 ${prefix}wink
+┃冬 ${prefix}poke
+║冬 ${prefix}dance
+┃冬 ${prefix}gasm              
+║冬 ${prefix}baka
+┃冬 ${prefix}tickle
+║冬 ${prefix}fox_girl
+┃冬 ${prefix}wallml
+║冬 ${prefix}milf
+╚❏
+
+╔─❒「 𝐷𝑜𝑤𝑛𝑙𝑜𝑎𝑑 」❒
+┃冬 ${prefix}dafontdl [ url ]
+║冬 ${prefix}igstory [ url ]
+┃冬 ${prefix}igdl [ url ]
+║冬 ${prefix}igdl2 [ url ]
+┃冬 ${prefix}igdl3 [ url ]
+║冬 ${prefix}mediafire [ url ]
+┃冬 ${prefix}play [ url ]
+║冬 ${prefix}tiktok [ url ]
+┃冬 ${prefix}ytmp3 [ url ]
+║冬 ${prefix}tomp3 [ url ]
+┃冬 ${prefix}ytmp4 [ url ]
+║冬 ${prefix}ytdownload [ url ]
+┃冬 ${prefix}twitter [ url ]
+║冬 ${prefix}twittermp3 [ url ]
+┃冬 ${prefix}tourl [ reply image ]
+╚❏
+
+╔─❒「 𝑆𝑒𝑎𝑟𝑐ℎ 」❒
+┃冬 ${prefix}brainly [ search ]
+║冬 ${prefix}dafontsearch [ search ]
+┃冬 ${prefix}gimage [ search ]
+║冬 ${prefix}google [ search ]
+┃冬 ${prefix}otakku [ search ]
+║冬 ${prefix}komiku [ search ]
+┃冬 ${prefix}chara [ search ]
+║冬 ${prefix}lirik [ search ]
+┃冬 ${prefix}igstalk [ search ]
+║冬 ${prefix}githubstalk [ search ]
+┃冬 ${prefix}pinterest [ search ]
+║冬 ${prefix}playstore [ search ]
+┃冬 ${prefix}wiki [ search ]
+║冬 ${prefix}ytsearch [ search ]
+╚❏
+
+╔─❒「 𝐵𝑢𝑔 」❒
+┃冬 ${prefix}x
+║冬 ${prefix}uni
+┃冬 ${prefix}xbug
+║冬 ${prefix}awara
+┃冬 ${prefix}arawav2
+║冬 ${prefix}bugv1
+┃冬 ${prefix}bugv2
+║冬 ${prefix}bugv3
+┃冬 ${prefix}bugv4
+║冬 ${prefix}bugv5
+┃冬 ${prefix}bugvn
+║冬 ${prefix}bugvn2
+┃冬 ${prefix}virtex
+║冬 ${prefix}virus1
+┃冬 ${prefix}virus2
+║冬 ${prefix}virus3
+┃冬 ${prefix}virus4
+║冬 ${prefix}virus5
+┃冬 ${prefix}virus6
+║冬 ${prefix}virus7
+┃冬 ${prefix}virus8
+║冬 ${prefix}virus9
+┃冬 ${prefix}virus10
+║冬 ${prefix}virus11
+┃冬 ${prefix}virus12
+║冬 ${prefix}virus13
+┃冬 ${prefix}buglink
+║冬 ${prefix}buggif
+┃冬 ${prefix}buggif2
+║冬 ${prefix}buggif3
+┃冬 ${prefix}buggif4
+║冬 ${prefix}buggif5
+┃冬 ${prefix}buginvite
+║冬 ${prefix}buginvitev2
+┃冬 ${prefix}bugmamaco
+║冬 ${prefix}bugvk
+┃冬 ${prefix}bugsunshine
+║冬 ${prefix}bugsilver
+┃冬 ${prefix}buggas
+║冬 ${prefix}bugcleitin
+┃冬 ${prefix}bugsungsw
+║冬 ${prefix}bugsungswv2
+┃冬 ${prefix}bugpdf
+║冬 ${prefix}bugpc
+┃冬 ${prefix}bugpc2
+║冬 ${prefix}vweb1
+┃冬 ${prefix}vweb2
+║冬 ${prefix}virtag
+┃冬 ${prefix}virtagv2
+║冬 ${prefix}virtagv3
+┃冬 ${prefix}virtagv4
+║冬 ${prefix}virtagv5
+┃冬 ${prefix}bugcacing
+║冬 ${prefix}jadivirkon
+┃冬 ${prefix}virkon
+║冬 ${prefix}bugstik
+┃冬 ${prefix}bugstikv2
+║冬 ${prefix}bughole
+┃冬 ${prefix}bugbutton
+║冬 ${prefix}bugbutton2
+┃冬 ${prefix}bugbutton3
+║冬 ${prefix}butrolilist
+┃冬 ${prefix}bugvideo
+║冬 ${prefix}bugvideov2
+┃冬 ${prefix}bugvideov3
+║冬 ${prefix}bugvideov4
+┃冬 ${prefix}bugtrolilist
+║冬 ${prefix}bugtroli [ jumlah ]
+┃冬 ${prefix}jadi [ teks ]
+║冬 ${prefix}jadiv2 [ teks ]
+┃冬 ${prefix}jadiv3 [ teks ]
+║冬 ${prefix}jafiv4 [ teks ]
+┃冬 ${prefix}catalog [ reply image ]
+║冬 ${prefix}bug1 [ teks ]
+┃冬 ${prefix}bug2 [ teks ]
+║冬 ${prefix}bug3 [ teks ]
+┃冬 ${prefix}bug4 [ teks ]
+║冬 ${prefix}bug5 [ teks ]
+┃冬 ${prefix}bug6 [ teks ]
+║冬 ${prefix}bug7 [ teks ]
+┃冬 ${prefix}kirin [ jumlah ]
+║冬 ${prefix}katsu [ jumlah ]
+┃冬 ${prefix}jadivirgam [ reply image ]
+║冬 ${prefix}jadivirgamv2 [ reply image ]
+┃冬 ${prefix}jadivirgamv3 [ reply image ]
+║冬 ${prefix}jadivirgamv4 [ reply image ]
+┃冬 ${prefix}jadivirvid [ reply video ]
+║冬 ${prefix}jadivirvidv2 [ reply video ]
+┃冬 ${prefix}jadivirvidv3 [ reply video ]
+║冬 ${prefix}jadivirvidv4 [ reply video ]
+╚❏
+
+┏━━⬣ 𝑇ℎ𝑎𝑛𝑘𝑠 𝑇𝑜 
+┃
+┃ 🔥 Pak Tzy
+┃ 🔥 Ranz
+┃ 🔥 Zextz
+┃ 🔥 Arifi Razaq OFC
+┃ 🔥 KurrXD
+┃ 🔥 Zeeone OFC
+┃ 🔥 Chandra
+┃ 🔥 Fan X7
+┃ 🔥 Dcode Dempa
+┃ 🔥 Yuzzu Kamiyaka
+┃ 🔥 Lexxy OFC
+┃ 🔥 Ramdani OFC
+┃ 🔥 Ridho
+┃
+┗━━━━━━━━━━━━━━━⬣️` 
+}
+exports.menunya = menunya
